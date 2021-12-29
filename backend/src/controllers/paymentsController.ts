6daca94164adb7e2fc50dbd8cd163956ebf158a6
marchewazz <mateuszooospot@gmail.com>
2021-12-30 00:12:36 +0100

    Orders are marked as paid only if amount in transaction is right

--- backend/src/controllers/paymentsController.ts
+++ backend/src/controllers/paymentsController.ts
@@ -8,13 +8,17 @@ class PaymentsController{
     public async updatePayments(): Promise<any> {
 
         axios.get(`${bankBackend}/transaction/historybill`, {data: {accountNumber: mainShopBill}}).then(async (res: any) => {
-            var bills = JSON.parse(res.data.bills);
-            for (var bill of bills){
-                const client = await pool.connect();
+            var transactions = JSON.parse(res.data.transactions);
+            const client = await pool.connect();
+            for (var transaction of transactions){
 
-                const query = `UPDATE orders SET "orderIsPaid"=true WHERE "orderID"=$1;`
+                var query = `SELECT "orderPrice" FROM orders WHERE "orderID" = $1;`;
+                var { rows } = await client.query(query, [Number(transaction.note)]);
+                if (rows[0].orderPrice == Number(transaction.amount)){
+                    query = `UPDATE orders SET "orderIsPaid"=true WHERE "orderID"=$1;`;
 
-                client.query(query, [Number(bill.note)]);
+                    client.query(query, [Number(transaction.note)]);
+                }
             }
         }).catch((e: any) => {
             console.log(e);
