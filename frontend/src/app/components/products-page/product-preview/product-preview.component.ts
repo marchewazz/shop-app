9a741e4aa75b5196911c51af329fe67d1ca198db
marchewazz <mateuszooospot@gmail.com>
2021-12-31 00:06:11 +0100

    Average rating is being shown in products page as well

--- frontend/src/app/components/products-page/product-preview/product-preview.component.ts
+++ frontend/src/app/components/products-page/product-preview/product-preview.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { CartService } from 'src/app/services/cartService/cart.service';
+import { CommentsService } from 'src/app/services/commentsService/comments.service';
 
 @Component({
   selector: 'app-product-preview',
@@ -11,10 +12,16 @@ import { CartService } from 'src/app/services/cartService/cart.service';
 export class ProductPreviewComponent implements OnInit {
 
   @Input() productData : any;
-  constructor(private router: Router, private cs: CartService) { }
+  average: number | undefined;
+  constructor(private router: Router, private cs: CartService, private comS: CommentsService) { }
 
   ngOnInit(): void {
     console.log(this.productData);
+    this.comS.getAverage({productID: this.productData.productID}).subscribe((res: any) =>{
+      console.log(res);
+      //EITHER I'M STUPID OR TYPESCRIPT BUT IT'S WEIRDEST LINE IN MY LIFE
+      this.average = Number(Number(res.average[0].avg).toFixed(2));
+    })
   }
 
   redirect(path: string): void{
