42299b096a89d89e7cf53a055ff8808a35037545
marchewazz <mateuszooospot@gmail.com>
2022-01-28 00:51:26 +0100

    List page is ready

--- frontend/src/app/components/list-page/list-page.component.ts
+++ frontend/src/app/components/list-page/list-page.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { ListsService } from 'src/app/services/listsService/lists.service';
+import { ProductsService } from 'src/app/services/productsService/products.service';
+import { CartService } from 'src/app/services/cartService/cart.service';
+
+import { shopCurrency } from 'src/app/utilities';
 
 @Component({
   selector: 'app-list-page',
@@ -12,9 +16,12 @@ export class ListPageComponent implements OnInit {
 
   isLoaded: boolean = false;
   list: any;
+  productsOnList: any;
+  listPrice: number = 0;
   userFirstName: string = "";
   createDate: string = "";
-  constructor(private route: ActivatedRoute, private ls: ListsService) { }
+  shopCurrency: string = shopCurrency;
+  constructor(private router: Router, private route: ActivatedRoute, private ls: ListsService, private ps: ProductsService, private cs: CartService) { }
 
   ngOnInit(): void {
     const listData = {
@@ -27,6 +34,18 @@ export class ListPageComponent implements OnInit {
       this.userFirstName = res.userFirstName;
       this.createDate = new Date(res.list.listCreateDate).toLocaleString();
       this.isLoaded = true;
+      this.ps.getProductsData(res.list.listProducts).then((value: any) => {
+        this.productsOnList = value;
+      })
+      setTimeout(() => this.listPrice = this.cs.calculatePrice(this.productsOnList), 100);
     })
+  }
+
+  createPath(path: string, param: any): string{
+    return path+param
+  }
+  
+  redirect(path: string): void{
+    this.router.navigate([path]);
   }
 }
