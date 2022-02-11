f23fbc61e49f5493267ec0a8bde57826b2850e7b
marchewazz <mateuszooospot@gmail.com>
2022-02-11 21:28:47 +0100

    Database URL update

--- backend/src/util/postgresConfig.ts
+++ backend/src/util/postgresConfig.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString : string | undefined = process.env.DATABASE_URL;
+const connectionString : string | undefined = process.env.DATABASE_URL || 'postgres://postgres:123@localhost:5432/shopApp';
 
 export default new Pool ({
     connectionString: connectionString,
