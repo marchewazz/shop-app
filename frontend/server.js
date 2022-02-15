5aad21690e2d15cea7b09b3769f9eba5f481315c
marchewazz <mateuszooospot@gmail.com>
2022-02-15 11:55:37 +0100

    Angular server update

--- frontend/server.js
+++ frontend/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/frontend"));
+app.use(express.static(path.join(__dirname + "/dist/frontend")));
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
