255b00c60378236807682e6dd904f7276afe7437
marchewazz <mateuszooospot@gmail.com>
2022-02-15 12:21:13 +0100

    Angular server update

--- frontend/server.js
+++ frontend/server.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname + "/dist/frontend")));
 
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
-});
+// app.get('/*', function(req,res) {
+//     res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
+// });
 
 app.listen(process.env.PORT || 5000, (req, res) => {
-    console.log(`fdsfs`);
+    console.log(`server works`);
 })
