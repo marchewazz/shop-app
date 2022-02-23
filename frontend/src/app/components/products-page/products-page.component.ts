6df3735a7a5577641affa7d08f6278fd29d9ae5c
marchewazz <mateuszooospot@gmail.com>
2022-02-23 16:31:45 +0100

    Sorting products on page

--- frontend/src/app/components/products-page/products-page.component.ts
+++ frontend/src/app/components/products-page/products-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 import { ProductsService } from 'src/app/services/productsService/products.service';
 
@@ -11,20 +12,44 @@ import { ProductsService } from 'src/app/services/productsService/products.servi
 export class ProductsPageComponent implements OnInit {
 
   products : any[] = [];
+  fetchedProducts: any[] = [];
   isLoaded : boolean = false;
+  showFilters: boolean = false;
   searchPhrase: string = "";
+
+  sortControl: FormControl = new FormControl();
+  availabilityControl: FormControl = new FormControl();
 
   constructor(private route: ActivatedRoute, private ps: ProductsService) { }
 
+
+  filterProducts(): void{
+
+    const sortParam: string = this.sortControl.value;
+
+    if(sortParam === "priceLowToHigh") this.products = this.ps.sortProductsByPrice(this.fetchedProducts, "asc")
+    if(sortParam === "priceHighToLow") this.products = this.ps.sortProductsByPrice(this.fetchedProducts, "desc")
+    if(sortParam === "nameAToZ") this.products = this.ps.sortProductsByName(this.fetchedProducts, "asc")
+    if(sortParam === "nameZToA") this.products = this.ps.sortProductsByName(this.fetchedProducts, "desc")
+    if(sortParam === "mostPopular") this.products = this.ps.sortByPopularity(this.fetchedProducts)
+    
+    if(this.availabilityControl.value) this.products = this.ps.getOnlyAvailableProducts(this.fetchedProducts)
+    else this.products = this.fetchedProducts 
+  }
+
   ngOnInit(): void {
+    this.sortControl.setValue("nameAToZ");
+    this.availabilityControl.setValue(false);
+
     this.ps.getAllProducts().subscribe((res: any) => {
       const searchPhrase = this.route.snapshot.paramMap.get('searchphrase')
       if(searchPhrase) {
         this.searchPhrase = searchPhrase;
-        this.products = this.ps.getProductsWithSearchedPhrase(res.products, searchPhrase);
+        this.fetchedProducts = this.ps.getProductsWithSearchedPhrase(res.products, searchPhrase);
       } else {
-        this.products = res.products;
+        this.fetchedProducts = res.products;
       }
+      this.filterProducts();
       this.isLoaded = true;
     });
   }
